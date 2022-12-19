a9f89ded20aa762a260fa8426b413d200bb62312
Prakash2311 <prakashdevta@gmail.com>
2022-12-19 14:03:24 +0200

    verifing user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,17 +1,6 @@
 import User from "../models/User.js"
 
 
-export const createUser =async (req,res,next)=> {
-
-    const newUser = new User(req.body)
-
-try{
-    const saveUser = await newUser.save()
-    res.status(200).json(saveUser)
-}catch(err){
-    next(err)
-}
-}
 
 export const updateUser =async (req,res,next)=> {
 
