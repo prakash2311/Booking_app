3f3bb4e30d0f13fab2ba5e84f370620e9d5cfe39
Prakash2311 <prakashdevta@gmail.com>
2022-12-19 11:31:05 +0200

    using bcrypt to hash pass

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,17 +1,23 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 
 export const register = async (req,res,next) => {
 
     try{
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
     const newUser = new User ({
         username:req.body.username,
         email:req.body.email,
-        password:req.body.password,
+        password:hash,
     })
 
-      await newUser.save()
-      res.status(200).send("User has been created")
+    await newUser.save()
+    res.status(200).send("User has been created")
+
     }catch( err ){
         next(err)
     }
