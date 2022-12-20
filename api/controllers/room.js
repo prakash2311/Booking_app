88bce155f0cf33b8b42d9c334593019724ab85c3
Prakash2311 <prakashdevta@gmail.com>
2022-12-20 10:56:58 +0200

    updating routes file

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -1,7 +1,6 @@
 import Room from "../models/Room.js";
 import Hotel from "../models/Hotel.js";
 import { createError} from "../utils/error.js";
-import Room from "../models/Room.js";
 
 export const createRoom = async (req, res, next) => {
    
