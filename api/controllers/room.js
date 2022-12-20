89e87e1a951fd7aba3179a748e36b52a92fdc3e9
Prakash2311 <prakashdevta@gmail.com>
2022-12-20 11:02:57 +0200

    solved

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -35,9 +35,14 @@ export const updateRoom =async (req,res,next)=> {
 export const deleteRoom =async (req,res,next)=> {
 
     try{
-        await Room.findByIdAndDelete(
-            req.params.id
-        );
+        await Room.findByIdAndDelete(req.params.id);
+        try{
+            await Hotel.findByIdAndUpdate(hotelId,{
+              $pull: { rooms : savedRoom._id},
+            })
+        }catch(err){
+         next(err)
+        }
         res.status(200).json("Room has been deleted..")
     }catch(err){
     next(err)
